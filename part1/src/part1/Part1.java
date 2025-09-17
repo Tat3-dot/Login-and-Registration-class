5889a3806c6cc0b8429e73d7085ff0f7106514a9
Tat3-dot <st10497663@rcconnect.edu.za>
2025-09-17 08:22:53 +0000

    Add files via upload

---
+++ part1/src/part1/Part1.java
@@ -0,0 +1,25 @@
+package registration;
+
+import javax.swing.JOptionPane;
+
+public class Registration{
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String username;
+    private String phoneNumber;
+    
+    public static void main(String args){
+      Registration registration= new registration();
+      registration.startRegistration();
+  
+    }
+    public void startRegistration(){
+     //Get first name
+     firstName = JOptionPane.showInputDialog("please enter first name");
+     
+     //Get surname
+     Surname = 
+    }
+}
+        
